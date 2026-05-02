784c6cd0d3399e7627e15df0226bfad3c452662d
ElMinouh <air7841@yahoo.fr>
2026-05-02 15:55:47 +0200

    Hotfix v8.2.4 : retrait watchdog SW qui empechait installation sur tablettes lentes

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v8.2.3';
+const CACHE_VERSION = 'v8.2.4';
 const CACHE_NAME = `odyssee-${CACHE_VERSION}`;
 
 // Ressources critiques précachées au premier chargement.
