3c5bcf2cce5f2ad2cfc31de017db03bcd4fd847d
ElMinouh <air7841@yahoo.fr>
2026-05-09 23:39:57 +0200

    v8.3.7 diag : logs console pour gif fin

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v8.3.6';
+const CACHE_VERSION = 'v8.3.7';
 const CACHE_NAME = `odyssee-${CACHE_VERSION}`;
 
 // Ressources critiques précachées au premier chargement.
