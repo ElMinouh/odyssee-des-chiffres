ffbdfa6ace989233b14c06b1f75cb96953cd38ba
ElMinouh <air7841@yahoo.fr>
2026-05-09 22:57:20 +0200

    v8.3.4 fix : centrage robuste GIF de fin de partie

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v8.3.3';
+const CACHE_VERSION = 'v8.3.4';
 const CACHE_NAME = `odyssee-${CACHE_VERSION}`;
 
 // Ressources critiques précachées au premier chargement.
