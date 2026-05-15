e1f5285ccb6ba3af71fa2bee6b47dd73078a7d61
ElMinouh <air7841@yahoo.fr>
2026-05-15 09:23:56 +0200

    v8.5.4 fix : suppression max-width qui bloquait l'animation carte + cache-buster logo accueil

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v8.5.3';
+const CACHE_VERSION = 'v8.5.4';
 const CACHE_NAME = `odyssee-${CACHE_VERSION}`;
 
 // Ressources critiques précachées au premier chargement.
