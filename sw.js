c83014c5ddeea098b402317469825e6d033fe95c
ElMinouh <air7841@yahoo.fr>
2026-08-01 19:40:29 +0200

    v11.6.15 - Etoffement Odyssee Primaire Histoire + collection Chroniques du Temps + fermeture lecteur tomes

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v11.6.14';
+const CACHE_VERSION = 'v11.6.15';
 const CACHE_NAME = `odyssee-${CACHE_VERSION}`;
 
 // Ressources critiques précachées au premier chargement.
