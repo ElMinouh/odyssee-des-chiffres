c1b42f2d376ab9de759274f664415c062e2ad830
ElMinouh <air7841@yahoo.fr>
2026-08-01 15:02:23 +0200

    v11.6.13 - Etoffement Odyssee Primaire Maths + livre bonus Nombres + boutons carnet explicites

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v11.6.12';
+const CACHE_VERSION = 'v11.6.13';
 const CACHE_NAME = `odyssee-${CACHE_VERSION}`;
 
 // Ressources critiques précachées au premier chargement.
