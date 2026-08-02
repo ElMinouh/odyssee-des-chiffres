82ffb106700deefbea9048488efcb767dc10892d
ElMinouh <air7841@yahoo.fr>
2026-08-02 14:12:24 +0200

    Chroniques du Temps: correction mythe 'bug' + 2 anecdotes (Rome, Marseillaise) - v11.7.1

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v11.7.0';
+const CACHE_VERSION = 'v11.7.1';
 const CACHE_NAME = `odyssee-${CACHE_VERSION}`;
 
 // Ressources critiques précachées au premier chargement.
