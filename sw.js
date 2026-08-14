d36ba02103f1bd9a3d7bda1d4dc3a694600d4ea2
ElMinouh <air7841@yahoo.fr>
2026-08-14 23:18:15 +0200

    v12.4.3 : fusion nav-icons.css/lot2-fixes.css dans styles.css + fix boussole mobile + nettoyage versions

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  */
 'use strict';
 
-const CACHE_VERSION = 'v12.4.2';
+const CACHE_VERSION = 'v12.4.3';
 const CACHE_NAME = `odyssee-${CACHE_VERSION}`;
 
 // Ressources critiques précachées au premier chargement.
@@ -22,8 +22,6 @@ const CRITICAL_URLS = [
   './index.html',
   './manifest.webmanifest',
   './css/styles.css',
-  './css/nav-icons.css',
-  './css/lot2-fixes.css',
   './js/01-core.js',
   './js/02-data.js',
   './js/03-figurines-data.js',
